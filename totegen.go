29ab12c7673711a4cdc4f464245683e53fc0dab1
Vangel Paronov <vangel@WhiteFrog-10.local>
2019-10-10 23:59:29 +0200

    comments & stuff

--- totegen.go
+++ totegen.go
@@ -1,15 +1,12 @@
 package main
 
-//Solution - ...............
+//Solution - contains combination of small and big totes
 type Solution struct {
 	smallTotes int
 	bigTotes   int
 }
 
-// 	small = 11.14
-// 	big   = 16.38
-
-//GetTotes - ..........
+//GetTotes - returns the optimal combination of small and big totes for given targetVolume
 func GetTotes(targetVolume float64, smallToteVolume float64, bigToteVolume float64) Solution {
 	memory := make(map[Solution]Solution)
 	return findSolution(
@@ -21,6 +18,9 @@ func GetTotes(targetVolume float64, smallToteVolume float64, bigToteVolume float
 	)
 }
 
+//
+// finds the optimal solution
+//
 func findSolution(solution Solution,
 	targetVolume float64,
 	smallToteVolume float64,
@@ -28,6 +28,7 @@ func findSolution(solution Solution,
 	memory map[Solution]Solution,
 ) Solution {
 
+	// if the volume is enough -  that is our solution
 	if calcVolume(solution, smallToteVolume, bigToteVolume) >= targetVolume {
 		return solution
 	}
@@ -35,18 +36,22 @@ func findSolution(solution Solution,
 	var sol1 Solution
 	var sol2 Solution
 
+	// variant 1 => add one small tote to the previous solution
 	v1 := Solution{smallTotes: solution.smallTotes + 1, bigTotes: solution.bigTotes}
+	// variant 2 => add one big tote to the previous solution
 	v2 := Solution{smallTotes: solution.smallTotes, bigTotes: solution.bigTotes + 1}
 
-	if cached, ok := memory[v1]; ok {
-		sol1 = cached
+	// check if we have already calculated the solution for variant 1
+	if cachedSolution, ok := memory[v1]; ok {
+		sol1 = cachedSolution
 	} else {
 		sol1 = findSolution(v1, targetVolume, smallToteVolume, bigToteVolume, memory)
 		memory[v1] = sol1
 	}
 
-	if cached, ok := memory[v2]; ok {
-		sol2 = cached
+	// the same for variant 2
+	if cachedSolution, ok := memory[v2]; ok {
+		sol2 = cachedSolution
 	} else {
 		sol2 = findSolution(v2, targetVolume, smallToteVolume, bigToteVolume, memory)
 		memory[v2] = sol2
@@ -55,6 +60,7 @@ func findSolution(solution Solution,
 	sol1Val := calcVolume(sol1, smallToteVolume, bigToteVolume)
 	sol2Val := calcVolume(sol2, smallToteVolume, bigToteVolume)
 
+	// the optimal solution is the one with minimum volume
 	if sol1Val <= sol2Val {
 		return sol1
 	}
