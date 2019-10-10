cb6b2dcf33cf62356a1a8c8c74a28a763ccc9b58
Vangel Paronov <vangel@WhiteFrog-10.local>
2019-10-10 23:48:25 +0200

    test

--- totegen_test.go
+++ totegen_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Test1(t *testing.T) {
-	targetVolume := 15000.44
+	targetVolume := 123.44
 	sol := GetTotes(targetVolume, 11.14, 16.38)
 
 	fmt.Printf("solution = %v, target = %f, solution volume = %f\n", sol, targetVolume, calcVolume(sol, 11.14, 16.38))
