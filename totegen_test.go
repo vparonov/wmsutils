67c9095a07cefb63f0dd6a7294c0e025361f2c84
Vangel Paronov <vangel@WhiteFrog-10.local>
2019-10-11 22:32:35 +0200

    checkpoint

--- totegen_test.go
+++ totegen_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Test1(t *testing.T) {
-	targetVolume := 1000.44
+	targetVolume := 12.44
 	sol1 := GetTotes(targetVolume, 11.14, 16.38)
 	sol2 := GetTotes(targetVolume, 11.14, 16.38)
 
